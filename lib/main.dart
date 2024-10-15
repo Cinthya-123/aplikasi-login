b812200697f2f613fd7ea9873d6a7a4b8eae878c
SherlyDindaTristanti23 <devycinthya1@gmail.com>
2024-10-15 20:02:20 +0700

    Memperbaiki

--- lib/main.dart
+++ lib/main.dart
@@ -44,131 +44,135 @@ class HalamanLogin extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color.fromARGB(255, 253, 250, 255),
-      body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image.asset(
-              'assets/images/login.png',
-              height: 200,
-              width: 200,
-            ),
-            SizedBox(height: 25),
-            Text(
-              "Login",
-              style: TextStyle(
-                fontSize: 30,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Username",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.left,
+        backgroundColor: Color.fromARGB(255, 253, 250, 255),
+        body: SafeArea(
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(height: 30),
+                Image.asset(
+                  'assets/images/login.png',
+                  height: 200,
+                  width: 200,
+                ),
+                SizedBox(height: 25),
+                Text(
+                  "Login",
+                  style: TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
                   ),
-                  SizedBox(height: 8),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      border:
-                          Border.all(color: Color.fromARGB(255, 89, 88, 88)),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Masukkan username",
-                          hintStyle: TextStyle(fontWeight: FontWeight.w300),
-                        ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Username",
                         style: TextStyle(
-                          color: const Color.fromARGB(255, 37, 37, 37),
-                        ),
-                        onChanged: (value) {
-                          username.value = value;
-                        },
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
                       ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.all(25.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "Password",
-                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
-                    textAlign: TextAlign.left,
-                  ),
-                  SizedBox(height: 8),
-                  Container(
-                    decoration: BoxDecoration(
-                      color: const Color.fromARGB(255, 255, 255, 255),
-                      border:
-                          Border.all(color: Color.fromARGB(255, 89, 88, 88)),
-                      borderRadius: BorderRadius.circular(12),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.only(left: 20.0),
-                      child: TextField(
-                        obscureText: true,
-                        decoration: InputDecoration(
-                          border: InputBorder.none,
-                          hintText: "Masukkan Password",
-                          hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                      SizedBox(height: 8),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          border: Border.all(
+                              color: Color.fromARGB(255, 89, 88, 88)),
+                          borderRadius: BorderRadius.circular(12),
                         ),
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: TextField(
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Masukkan username",
+                              hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                            ),
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 37, 37, 37),
+                            ),
+                            onChanged: (value) {
+                              username.value = value;
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(25.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Password",
                         style: TextStyle(
-                          color: const Color.fromARGB(255, 37, 37, 37),
+                            fontSize: 16, fontWeight: FontWeight.bold),
+                        textAlign: TextAlign.left,
+                      ),
+                      SizedBox(height: 8),
+                      Container(
+                        decoration: BoxDecoration(
+                          color: const Color.fromARGB(255, 255, 255, 255),
+                          border: Border.all(
+                              color: Color.fromARGB(255, 89, 88, 88)),
+                          borderRadius: BorderRadius.circular(12),
                         ),
-                        onChanged: (value) {
-                          password.value = value;
-                        },
+                        child: Padding(
+                          padding: const EdgeInsets.only(left: 20.0),
+                          child: TextField(
+                            obscureText: true,
+                            decoration: InputDecoration(
+                              border: InputBorder.none,
+                              hintText: "Masukkan Password",
+                              hintStyle: TextStyle(fontWeight: FontWeight.w300),
+                            ),
+                            style: TextStyle(
+                              color: const Color.fromARGB(255, 37, 37, 37),
+                            ),
+                            onChanged: (value) {
+                              password.value = value;
+                            },
+                          ),
+                        ),
                       ),
-                    ),
+                    ],
                   ),
-                ],
-              ),
-            ),
-            SizedBox(height: 10),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 25.0),
-              child: Obx(() => GestureDetector(
-                    onTap: isLoading.value ? null : login,
-                    child: Container(
-                      padding: EdgeInsets.symmetric(vertical: 10.0),
-                      decoration: BoxDecoration(
-                        color: Color.fromRGBO(56, 40, 87, 1),
-                        borderRadius: BorderRadius.circular(12),
-                      ),
-                      child: Center(
-                        child: isLoading.value
-                            ? CircularProgressIndicator(
-                                color: Colors.white,
-                              )
-                            : Text(
-                                "Login",
-                                style: TextStyle(
+                ),
+                SizedBox(height: 10),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25.0),
+                  child: Obx(() => GestureDetector(
+                        onTap: isLoading.value ? null : login,
+                        child: Container(
+                          padding: EdgeInsets.symmetric(vertical: 10.0),
+                          decoration: BoxDecoration(
+                            color: Color.fromRGBO(56, 40, 87, 1),
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Center(
+                            child: isLoading.value
+                                ? CircularProgressIndicator(
                                     color: Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16),
-                              ),
-                      ),
-                    ),
-                  )),
+                                  )
+                                : Text(
+                                    "Login",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                        fontSize: 16),
+                                  ),
+                          ),
+                        ),
+                      )),
+                ),
+              ],
             ),
-          ],
-        ),
-      ),
-    );
+          ),
+        ));
   }
 }
